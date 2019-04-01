fd806607193517f1cf771b96f5760e5cc3bc59ae
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-01 11:40:00 -0500

    [INFO] - Imágenes: Validaciones antes de subir imagen

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -11,7 +11,7 @@ import Swal from 'sweetalert2';
 })
 export class DetalleComponent implements OnInit {
   cliente: Cliente;
-  private imageSelected: File; // Atributo propio de la clase lo dejamos como privado
+  private imageSelected: File = null; // Atributo propio de la clase lo dejamos como privado
 
   constructor(private clienteService: ClienteService,
               private activatedRoute: ActivatedRoute) { }
@@ -33,13 +33,26 @@ export class DetalleComponent implements OnInit {
   selectImage(event) {
     this.imageSelected = event.target.files[0];
     console.log(this.imageSelected);
+
+    if(this.imageSelected.type.indexOf('image') < 0) {
+      Swal.fire('Error seleccionar imagen:', 'El archivo debe ser una imagen.', 'error');
+      this.imageSelected = null;
+    }
+
   }
 
   uploadFile() {
+    if(!this.imageSelected) {
+      Swal.fire('Error Upload:', 'Debe seleccionar una imagen.', 'error');
+    } else {
+
     this.clienteService.uploadImage(this.imageSelected, this.cliente.id).subscribe( cliente => {
       this.cliente = cliente;
       Swal.fire("La foto se ha subido completamente!", `La foto se ha subido con éxito: ${this.cliente.image}`, 'success');
+      this.imageSelected = null;
     });
+
+    }
 
   }
 
