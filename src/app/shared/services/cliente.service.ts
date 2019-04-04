97573b7cd3553ec35407081111ed6ded3162940d
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-04 13:12:56 -0500

    [INFO] - Autenticación: Manejo de error no autorizado/recurso prohibido (401-403) y redirección al login.

--- src/app/shared/services/cliente.service.ts
+++ src/app/shared/services/cliente.service.ts
@@ -18,8 +18,21 @@ export class ClienteService {
   constructor(private http: HttpClient,
     private router: Router) { }
 
+  private isNotAuthorized(e): boolean {
+    if(e.status == 401 || e.status == 403) { // 401: Unauthorized => No autorizado | 403: Forbidden => Recurso prohibido
+      this.router.navigate(['/login']);
+      return true;
+    }
+    return false;
+  }
+
   getRegiones(): Observable<Region[]> {
-    return this.http.get<Region[]>(this.urlEndPoint + '/regiones');
+    return this.http.get<Region[]>(this.urlEndPoint + '/regiones').pipe(
+      catchError(e => {
+        this.isNotAuthorized(e);
+        return throwError(e);
+      })
+    );
   }
 
   getClientes(page: number): Observable<any> {
@@ -64,6 +77,10 @@ export class ClienteService {
     return this.http.post(this.urlEndPoint, client, { headers: this.httpHeaders }).pipe(
       map((response: any) => response.cliente as Cliente),
       catchError(e => {
+        if(this.isNotAuthorized(e)) {
+          return throwError(e);
+        }
+
         if (e.status == 400) {
           return throwError(e);
         }
@@ -77,6 +94,11 @@ export class ClienteService {
   getCliente(id: number): Observable<Cliente> {
     return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
       catchError(e => {
+
+        if(this.isNotAuthorized(e)) {
+          return throwError(e);
+        }
+
         this.router.navigate(['/clientes']);
         console.error(e.error.mensaje);
         Swal.fire('Error al editar', e.error.mensaje, 'error');
@@ -89,6 +111,11 @@ export class ClienteService {
   update(client: Cliente): Observable<any> {
     return this.http.put<any>(`${this.urlEndPoint}/${client.id}`, client, { headers: this.httpHeaders }).pipe(
       catchError(e => {
+
+        if(this.isNotAuthorized(e)) {
+          return throwError(e);
+        }
+
         if (e.status == 400) {
           return throwError(e);
         }
@@ -102,6 +129,11 @@ export class ClienteService {
   delete(id: number): Observable<Cliente> {
     return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, { headers: this.httpHeaders }).pipe(
       catchError(e => {
+
+        if(this.isNotAuthorized(e)) {
+          return throwError(e);
+        }
+
         console.error(e.error.mensaje);
         Swal.fire(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
@@ -118,7 +150,13 @@ export class ClienteService {
       reportProgress: true
     });
 
-    return this.http.request(req);
+    return this.http.request(req).pipe(
+      catchError(e => {
+        this.isNotAuthorized(e);
+        return throwError(e);
+      })
+    );
+    
   }
 
 }
