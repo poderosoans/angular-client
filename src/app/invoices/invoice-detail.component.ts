d2af9e54b7faa2b5a96752875fd4f89ad773d7dc
Roger Pomacarhua <poderosoans@hotmail.com>
2019-04-09 12:41:46 -0500

    [INFO] - Sistema Facturación: Añadiendo el detalle de la Factura.

--- src/app/invoices/invoice-detail.component.ts
+++ src/app/invoices/invoice-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from '../shared/services/invoice.service';
+import { Invoice } from '../shared/model/invoice';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-detail',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceDetailComponent implements OnInit {
 
-  constructor() { }
+  invoice: Invoice;
+  title: string = 'Factura';
+
+  constructor(private invoiceService: InvoiceService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let id = +params.get('id');
+      this.invoiceService.getInvoice(id).subscribe(invoice => {
+        this.invoice = invoice;
+      })
+    })
   }
 
 }
